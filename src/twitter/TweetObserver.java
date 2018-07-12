7a747155eedd37ae490b1624d82d5b3f90b3a208
fergo <fergo@DESKTOP-8CRVAQB>
2018-07-12 07:37:18 -0700

    removed add tweet in observer update method

--- src/twitter/TweetObserver.java
+++ src/twitter/TweetObserver.java
@@ -8,7 +8,6 @@ public class TweetObserver implements Observer {
 	
 	public void update(Observable s, Object arg) {
 		if (s instanceof User) {
-			((User) s).addTweet((String) arg);
 			System.out.println("Tweet added!");
 			System.out.println(((User) s).getID() + ": " + ((User) s).getLastTweet());
 		}
