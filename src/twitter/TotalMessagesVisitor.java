e90adafe2632a9d5b518320145a6bf70523768fc
fergo <fergo@10.110.68.17>
2018-07-12 17:46:41 -0700

    New class for total number of messages.

--- src/twitter/TotalMessagesVisitor.java
+++ src/twitter/TotalMessagesVisitor.java
@@ -1,5 +1,20 @@
 package twitter;
 
-public class TotalMessagesVisitor {
+public class TotalMessagesVisitor implements Visitor {
+	
+	public TotalMessagesVisitor() {
+		
+	}
 
+	@Override
+	public void atUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atGroup(Group user) {
+		// TODO Auto-generated method stub
+		
+	}
 }
