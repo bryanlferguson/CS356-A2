77bbc6ebb8d16b4fbc230afebcc9b57780b6744e
fergo <fergo@10.110.68.17>
2018-07-12 19:36:04 -0700

    GUI work

--- src/twitter/AdminView.java
+++ src/twitter/AdminView.java
@@ -1,10 +1,14 @@
 package twitter;
 
 import java.awt.BorderLayout;
+import java.awt.Frame;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.JTree;
 
 public class AdminView {
 	
@@ -12,11 +16,14 @@ public class AdminView {
 	private JFrame frame;
 	private JTextField newUserID, newGroupID;
 	private JPanel left;
+	private JTree tree;
 	private Group root;
 	
 	private AdminView() {
 		root = new Group("root");
 		initFrame(2000, 1200);
+		
+		
 	}
 	
 	public static AdminView getInstance() {
@@ -32,12 +39,21 @@ public class AdminView {
 	
 	private void initFrame(int width, int height) {
 		frame = new JFrame("Admin View");
-		frame.setVisible(true);
-		frame.setSize(width, height);
+		frame.setSize(2000, 1200);
+        frame.setLocation(100, 50);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
 	}
 	
 	private void initTreeView() {
-		left = new JPanel(new BorderLayout());
+		left = new JPanel();
+		left.setLayout(new BorderLayout());
+
+		tree = new JTree();
+		tree.setVisible(true);
+
+		left.add(tree);
+		
 		frame.add(left);
 	}
 	
